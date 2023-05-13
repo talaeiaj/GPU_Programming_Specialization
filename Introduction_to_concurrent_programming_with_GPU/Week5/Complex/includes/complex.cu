06ac9b9ff5cd63bb344ec14cc9e21c284ed19265
ajtalaei <ajtalaei@gmail.com>
2023-05-13 01:10:15 -0400

    some minor changes

--- Introduction_to_concurrent_programming_with_GPU/Week5/Complex/includes/complex.cu
+++ Introduction_to_concurrent_programming_with_GPU/Week5/Complex/includes/complex.cu
@@ -1,23 +1,3 @@
-#include <stdio.h>
-
-#include <tuple>
-
-// For the CUDA runtime routines (prefixed with "cuda_")
-#include <cuda_runtime.h>
-
-__global__ void vectorMult(const float *A, const float *B, float *C, int numElements);
-float deviceMultiply(float a, float b);
-std::tuple<float *, float *, float *> allocateHostMemory(int numElements);
-std::tuple<float *, float *, float *> allocateDeviceMemory(int numElements);
-void copyFromHostToDevice(float *h_A, float *h_B, float *d_A, float *d_B, int numElements);
-void executeKernel(float *d_A, float *d_B, float *d_C);
-__host__ void copyFromDeviceToHost(float *d_C, float *h_C, int numElements);
-void deallocateMemory(float *h_A, float *h_B, float *h_C, float *d_A, float *d_B, float *d_C);
-void cleanUpDevice();
-void performTest(float *h_A, float *h_B, float *h_C, int numElements);
-
-
-
 /*
  * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
  *
@@ -37,7 +17,7 @@ void performTest(float *h_A, float *h_B, float *h_C, int numElements);
  * of the programming guide with some additions like error checking.
  */
 
-#include "complex.h"
+#include "../not_includes/complex.h"
 
 /*
  * CUDA Kernel Device code
--- Introduction_to_concurrent_programming_with_GPU/Week5/Complex/not_includes/complex.h
+++ Introduction_to_concurrent_programming_with_GPU/Week5/Complex/not_includes/complex.h
@@ -6,11 +6,11 @@
 
 __global__ void vectorMult(const float *A, const float *B, float *C, int numElements);
 float deviceMultiply(float a, float b);
-__host__ std::tuple<float *, float *, float *> allocateHostMemory(int numElements);
-__host__ std::tuple<float *, float *, float *> allocateDeviceMemory(int numElements);
-__host__ void copyFromHostToDevice(float *h_A, float *h_B, float *d_A, float *d_B, int numElements);
-__host__ void executeKernel(float *d_A, float *d_B, float *d_C);
+std::tuple<float *, float *, float *> allocateHostMemory(int numElements);
+std::tuple<float *, float *, float *> allocateDeviceMemory(int numElements);
+void copyFromHostToDevice(float *h_A, float *h_B, float *d_A, float *d_B, int numElements);
+void executeKernel(float *d_A, float *d_B, float *d_C);
 __host__ void copyFromDeviceToHost(float *d_C, float *h_C, int numElements);
-__host__ void deallocateMemory(float *h_A, float *h_B, float *h_C, float *d_A, float *d_B, float *d_C);
-__host__ void cleanUpDevice();
-__host__ void performTest(float *h_A, float *h_B, float *h_C, int numElements);
+void deallocateMemory(float *h_A, float *h_B, float *h_C, float *d_A, float *d_B, float *d_C);
+void cleanUpDevice();
+void performTest(float *h_A, float *h_B, float *h_C, int numElements);
